e787a4230f4beb7e06fda2599addbec5be83c043
Polina Moiseeva <moiseeva.polina@gmail.com>
2022-02-23 19:32:41 +0400

    Comment changes for now

--- snake.py
+++ snake.py
@@ -1,4 +1,6 @@
-class Snake:
+'''class Snake:
     def __init__(self, matrix, x, y, length):
-        matrix.showSnake(x, y, 5, length)
+        matrix.showSnake(x, y, length)
         self.body = [0]*length
+        for x in self.body:
+            x = [x, y]'''
